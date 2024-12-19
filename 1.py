790f07397263947c94cf970bdd8fd67cf851994e
sairam-peduri <22b81a67c5@cvr.ac.in>
2024-12-19 22:51:38 +0530

    Initial commit

---
+++ 1.py
@@ -0,0 +1,6 @@
+x=float(input("Enter the distance in feet : "))
+
+print("Distance in feet : ",x)
+print("Distance in inches : ",12*x)
+print("Distance in yards : ",x/3)
+print("Distance in miles : ",x/5280)
